6e4a6397eaf51d866e536e7284732b81704f5fbe
Chika825 <lightgreen726@gmail.com>
2024-05-28 15:24:45 +0900

    盤

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,42 @@ import styles from './index.module.css';
 
 const Home = () => {
   const [samplePos, setSamplePos] = useState(0);
-  const [bombMap, setbombMap] = useState(0);
+  const [bombMap, setbombMap] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
   // 0,1 bomb aru nai
   const [userInputs, setUserInputs] = useState(0);
   // 0~3でクリック、右クリックなど
-  const board:number[][]=[]
+  // const board:number[][]=[]
   //board.push
   //再帰関数→0連鎖 Reactの副作用 (useEffect→副作用を隔離 時計を作るとき使う)
   // 要件: Replayはいらない レべル＆カスタム 機能すべて useStateを少なくなる
-  console.log(samplePos);
+  const clickHandler = (x: number, y: number) => {
+    console.log(y, x);
+  };
   return (
     <div className={styles.container}>
-      <div
-        className={styles.sampleStyle}
-        style={{ backgroundPosition: `${samplePos * -30}px 0px` }}
-      />
-      <button onClick={() => setSamplePos((P) => (P + 1) % 14)} />
+      <div className={styles.gameBoard}>
+        <div className={styles.boardStyle}>
+          {bombMap.map((row, y) =>
+            row.map((flag, x) => (
+              <div
+                className={styles.cellStyle}
+                key={`${x}-${y}`}
+                onClick={() => clickHandler(x, y)}
+              />
+            )),
+          )}
+        </div>
+      </div>
     </div>
   );
 };
